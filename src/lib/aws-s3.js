bd9c89cb6f05d4b1783765a268de4cf6dc1097a4
Sm-Ashraful <smashraful.dev@gmail.com>
2024-01-25 18:32:13 +0600

    env variable name change

--- src/lib/aws-s3.js
+++ src/lib/aws-s3.js
@@ -1,14 +1,12 @@
-import { S3Client  } from "@aws-sdk/client-s3";
-
+import { S3Client } from "@aws-sdk/client-s3";
 
 // Set up AWS credentials
 const s3Client = new S3Client({
   region: process.env.REGION,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
-
 
 export { s3Client };
